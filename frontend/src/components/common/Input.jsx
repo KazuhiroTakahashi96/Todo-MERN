962db7ff98ef060ef0658945f79b12ee7f65779f
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-09-30 23:46:16 +0900

    added button, input components

---
+++ frontend/src/components/common/Input.jsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <input
+      type="text"
+      placeholder="TODOを入力"
+      className="input input-bordered w-full max-w-xs"
+    />
+  );
+};
+
+export default Input;

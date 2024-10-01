b99007b8eea83310ba3dd4d352550b4b79a38243
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-01 23:52:58 +0900

    ダミーデータで仮実装1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,23 @@ import Button from "./components/common/Button";
 import Input from "./components/common/Input";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "勉強",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "睡眠",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "食事",
+      isCompleted: false,
+    },
+  ]);
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -11,6 +27,13 @@ function App() {
       <div className="flex items-center justify-center gap-4">
         <Input />
         <Button />
+      </div>
+      <div className="mt-20">
+        {todos.map((todo) => (
+          <p key={todo.id} className="mb-5">
+            {todo.text}
+          </p>
+        ))}
       </div>
     </div>
   );

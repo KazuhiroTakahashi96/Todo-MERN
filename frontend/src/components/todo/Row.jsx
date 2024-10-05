d25a9ad303433f2386f1a70f8284ac5472002059
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-05 22:22:52 +0900

    table row修正

--- frontend/src/components/todo/Row.jsx
+++ frontend/src/components/todo/Row.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import Button from "../common/Button";
+import Badge from "../common/Badge";
+
 const Row = ({ text, isCompleted }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => setIsEdit(true);
+
+  const handleUpdate = () => setIsEdit(false);
   return (
     <tr>
       <td>{text}</td>
-      <td>{isCompleted === true ? "完了" : "未完了"}</td>
-      <td>edit</td>
+      <td>
+        <Badge
+          text={isCompleted === true ? "完了" : "未完了"}
+          boolean={isCompleted}
+        />
+      </td>
+      <td>
+        {isEdit ? (
+          <Button text={"更新"} onClick={handleUpdate} />
+        ) : (
+          <Button text={"編集"} onClick={handleEdit} />
+        )}
+      </td>
     </tr>
   );
 };

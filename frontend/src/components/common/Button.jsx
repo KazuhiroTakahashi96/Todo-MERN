cf218dbc3f1115e5d56993e27332e01a77e1ed1b
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-07 23:50:32 +0900

    buttonコンポーネントの修正

--- frontend/src/components/common/Button.jsx
+++ frontend/src/components/common/Button.jsx
@@ -1,12 +1,72 @@
-const Button = ({ text, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className="btn btn-primary text-white font-medium"
-    >
-      {text}
-    </button>
-  );
+const Button = ({ text, onClick, styleType }) => {
+  switch (styleType) {
+    case "secondary":
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-secondary  text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+
+    case "accent":
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-accent text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+
+    case "ghost":
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-ghost text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+
+    case "link":
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-link text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+
+    case "warning":
+      return (
+        <button onClick={onClick} className="btn btn-warning font-medium">
+          {text}
+        </button>
+      );
+
+    case "error":
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-error text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+
+    default:
+      return (
+        <button
+          onClick={onClick}
+          className="btn btn-primary text-white font-medium"
+        >
+          {text}
+        </button>
+      );
+  }
 };
 
 export default Button;

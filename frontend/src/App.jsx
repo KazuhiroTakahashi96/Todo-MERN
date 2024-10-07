4c3d4f6890ca660acfa74ae92f45a7138912787c
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-07 23:52:16 +0900

    トーストの追加（まだコメントアウト）

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Button from "./components/common/Button";
 import Input from "./components/common/Input";
 import Table from "./components/todo/Table";
+import Toast from "./components/common/Toast";
 
 function App() {
   const [input, setInput] = useState("");
@@ -39,6 +40,7 @@ function App() {
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center py-10 px-20">
+      {/* <Toast message={"Hello World!!"} /> */}
       <div className="flex items-center justify-center gap-4">
         <Input input={input} setInput={setInput} />
         <Button text={"追加"} onClick={handleAddTodo} />

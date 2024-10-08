2c8b607dd2c87bb065247a5d79e116ed5424e4e4
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-08 21:10:20 +0900

    BE: CRUDのAPI実装

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 import Todo from "../models/todo.model.js";
 
+// 参考：https://www.mongodb.com/developer/languages/javascript/getting-started-with-mongodb-and-mongoose/
+// mongoose doc: https://mongoosejs.com/docs/queries.html
+
 export const getTodos = async (req, res) => {
+  // corsエラー対応：https://qiita.com/mtoutside/items/cee708841cad7e02f85c
+  res.set({ "Access-Control-Allow-Origin": "*" });
+
   try {
-    const allTodo = await Todo.find({}); // ref:https://mongoosejs.com/docs/api/model.html#Model.find()
-    console.log(allTodo);
+    // ref:https://mongoosejs.com/docs/api/model.html#Model.find()
+    const allTodo = await Todo.find({}).sort({ updatedAt: "desc" });
     res.status(200).json({ success: true, data: allTodo });
   } catch (error) {
     console.error("Error in the fetching process: ", error.message);
@@ -24,7 +30,6 @@ export const createTodo = async (req, res) => {
   try {
     const newTodo = await Todo.create(todo);
 
-    console.log(newTodo);
     res.status(201).json({ success: true, data: newTodo });
   } catch (error) {
     console.error("Error in the creating process: ", error.message);
@@ -32,6 +37,42 @@ export const createTodo = async (req, res) => {
   }
 };
 
-export const updateTodo = async (req, res) => {};
+export const updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const todo = req.body;
 
-export const deleteTodo = async (req, res) => {};
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Todo Id" });
+  }
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      {
+        isCompleted: todo.isCompleted,
+      },
+      { new: true }
+    );
+    console.log(updatedTodo);
+    res.status(200).json({ success: true, data: updatedTodo });
+  } catch (error) {
+    console.error("Error in the updating process: ", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Todo Id" });
+  }
+
+  try {
+    await Todo.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Todo is deleted" });
+  } catch (error) {
+    console.error("Error in the deleting process: ", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};

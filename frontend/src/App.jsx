5055afb8ff3ee9dd947c3a112db3f2f00c9c9bbb
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-08 12:19:20 +0900

    props修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,8 +42,8 @@ function App() {
     <div className="h-screen w-screen flex flex-col items-center justify-center py-10 px-20">
       {/* <Toast message={"Hello World!!"} /> */}
       <div className="flex items-center justify-center gap-4">
-        <Input input={input} setInput={setInput} />
-        <Button text={"追加"} onClick={handleAddTodo} />
+        <Input input={input} setInput={setInput} placeholder={"TODOを入力"} />
+        <Button label={"追加"} onClick={handleAddTodo} />
       </div>
       <div className="h-full w-full mt-14 px-10 overflow-auto">
         <Table todos={todos} />

6a9492a5c32dd02f7da6e6d8f99ca9bd96c97241
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-08 15:43:41 +0900

    DBとの接続

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import todoRoutes from "./routes/todo.route.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -13,5 +14,6 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use("/api/todos", todoRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port: ${PORT}`);
 });

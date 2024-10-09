850c23af77d3ea7a5e744caecb41d55e1743a0a8
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-10-09 22:02:20 +0900

    todoの作成とトーストの表示非表示の実装

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useTodoStore } from "./store/todo";
+import { useToastStore } from "./store/toast";
+
 import Button from "./components/common/Button";
 import Input from "./components/common/Input";
 import Table from "./components/todo/Table";
@@ -6,47 +9,41 @@ import Toast from "./components/common/Toast";
 
 function App() {
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "勉強",
-      isCompleted: true,
-    },
-    {
-      id: 2,
-      text: "睡眠",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      text: "食事",
-      isCompleted: false,
-    },
-  ]);
+  const { createTodo } = useTodoStore();
+  const { status, showToast, hideToast } = useToastStore();
 
-  const handleAddTodo = () => {
+  const handleAddTodo = async () => {
     if (!input) return;
-    setTodos((prev) => [
-      ...prev,
-      {
-        id: prev.length + 1,
-        text: input,
-        isCompleted: false,
-      },
-    ]);
 
-    setInput("");
+    const newTodo = {
+      task: input,
+      isCompleted: false,
+    };
+
+    const res = await createTodo(newTodo);
+
+    if (res.success) {
+      showToast("TODOの登録に成功しました", "success");
+      hideToast();
+      setInput("");
+    } else {
+      console.error("Failed to create new todo");
+      showToast("TODOの登録に失敗しました", "error");
+      hideToast();
+    }
   };
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center py-10 px-20">
-      {/* <Toast message={"Hello World!!"} /> */}
+      {status.isShow ? (
+        <Toast message={status.message} type={status.type} />
+      ) : null}
       <div className="flex items-center justify-center gap-4">
         <Input input={input} setInput={setInput} placeholder={"TODOを入力"} />
         <Button label={"追加"} onClick={handleAddTodo} />
       </div>
       <div className="h-full w-full mt-14 px-10 overflow-auto">
-        <Table todos={todos} />
+        <Table />
       </div>
     </div>
   );
